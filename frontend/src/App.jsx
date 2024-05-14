d7bd2a5dbdd1dedb0b3ef42fd0d60c543eeb827f
Erin Robinson <r.erin2546@gmail.com>
2024-05-15 00:11:28 +0930

    adding app.jsx and navbar.jsx styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,40 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { getUser } from "./utilities/users-service";
 import AuthPage from "./pages/AuthPage";
-import bookImage2 from "../public/images/bookImage2.jpg";
 import { useState } from "react";
 import "./styles.css";
 import BookBrowse from "./components/BookBrowse";
 import Navbar from "./components/Navbar";
 import LoginForm from "./forms/LoginForm";
 import Home from "./components/Home";
+import help3 from '../public/help3.jpg'
+
 
 // Updated import statement
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const [user, setUser] = useState(getUser());
 
-  console.log(user)
+  console.log(user);
 
   return (
-    <>
-      
-      { user ?
+    <div className="white">
+      {" "}
+      {/* Use Tailwind CSS yellow-100 color */}
       <Router>
         <div>
           <Navbar />
+          <div className="relative">
+  <div className="flex-grow flex items-center justify-center bg-white px-10">
+    <img src={help3} alt="Book Banner" className="w-full h-auto max-h-80" />
+  </div>
+  <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+    <p className="text-white text-2xl font-bold"></p>
+  </div>
+</div>
+
+
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/allBooks" element={<BookBrowse />} />
@@ -31,10 +42,7 @@ function App() {
           </Routes>
         </div>
       </Router>
-        :
-        <AuthPage setUser={setUser} />
-      }
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
+import logo2 from "../../public/logo2.jpg"
+
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white" style={{ fontFamily: 'Pacifico, cursive', fontSize: '1.5rem', marginRight: 'auto' }}>PlotPoint Books.</div>
+    <nav className="bg-white py-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <img src={logo2} alt="Logo" className="h-10 mr-2" /> {/* Adjust the height as needed */}
+          <div className="text-black font-semibold text-lg underline" style={{ fontFamily: 'Pacifico, cursive', fontSize: '1.5rem' }}>PlotPoint Books.</div>
+        </div>
         <div className="flex">
-          <Link to="/allBooks" className="text-white font-bold mr-4">Bookstore</Link>
-          <Link to="/login" className="text-white font-bold mr-4">Login</Link>
-          <span className="text-white font-bold mr-4">Events</span>
-          <span className="text-white font-bold mr-4">About</span>
-          <span className="text-white font-bold mr-4">Contact</span>
-          <FaShoppingCart className="text-white" />
+          <Link to="/allBooks" className="text-black font-bold mr-4">Bookstore</Link>
+          <Link to="/login" className="text-black font-bold mr-4">Login</Link>
+          <span className="text-black font-bold mr-4">Events</span>
+          <span className="text-black font-bold mr-4">About</span>
+          <span className="text-black font-bold mr-4">Contact</span>
+          <FaShoppingCart className="text-black" />
         </div>
       </div>
     </nav>
@@ -21,6 +26,7 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
 
